40c68ab7b7c48b6f282227bd3a80358728a776c1
Farothtc <mvkirimi@gmail.com>
2024-11-11 18:18:27 +0300

    Half-done forms added to page

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,18 @@ import Link from "next/link";
 export default function Page() {
   return (
     <>
-      <h3>Hello</h3>
-      <Link href={"/login"}>Login here</Link>
+      <p className="text-center text-2xl text-gray-600 mb-5 ">
+        Don&rsquo;t have an account ? <strong>Create One</strong>
+      </p>
+      <form action=".">
+        <div className="mb-3">
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full max-w-xs bg-base-100"
+          />
+        </div>
+      </form>
     </>
   );
 }
